fdc72a7a0475d11f6e42ce281ae848d0a9c9c339
Vladislav Momatau <vladislav.momatau@itechart-group.com>
2021-12-05 22:10:03 +0300

    Fix typo.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[ show edit update destroy, like ]
+  before_action :set_post, only: %i[ show edit update destroy like ]
 
   # GET /posts or /posts.json
   def index
