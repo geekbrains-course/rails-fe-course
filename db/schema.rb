a8fe9a22195a63fa17cab65f46e08bc390b538e6
Vladislav Momatau <vladislav.momatau@itechart-group.com>
2021-12-08 21:05:26 +0300

    Use pg db adapter.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_11_02_161547) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "name"
     t.string "title"
