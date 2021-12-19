b5b3863f882de9b27c48b36b576d74831c3db614
PFdeve <pavel.fedarovich@itechart-group.com>
2021-12-19 03:16:04 +0300

    Init Stimulus

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,20 +1,21 @@
 import React from "react";
-import { render } from "react-dom";
-import App from "../src/components/App";
+import { render } from "react-dom"
+import App from "../src/components/App"
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap';
+import 'bootstrap'
 import "trix"
 import "@rails/actiontext"
 import "src/profile.js"
 import "src/post.js"
+import "controllers"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
 document.addEventListener("DOMContentLoaded", () => {
-	render(<App/>, document.body.appendChild(document.createElement("div")));
+	render(<App />, document.body.appendChild(document.createElement("div")));
 });
